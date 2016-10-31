2e8d1df44f95f289532d8bd3f65252ddd9367aac
U959139 <aimed@hotmail.fr>
2016-10-31 08:45:59 +0100

    DEUCE & ADVANTAGE Status

---
+++ jeu.tennis/src/test/java/com/socgen/lyxor/kata/tennis/JeuTest.java
@@ -0,0 +1,42 @@
+package com.socgen.lyxor.kata.tennis;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.socgen.lyxor.kata.tennis.impl.JoueurImpl;
+
+public class JeuTest {
+	private Jeu				jeu;
+	private final Joueur	j1	= new JoueurImpl("YMB");
+	private final Joueur	j2	= new JoueurImpl("LYXOR");
+
+	@Before
+	public void init() {
+		j1.resetScore();
+		j2.resetScore();
+		jeu = new JeuImpl(j1, j2, 1);
+	}
+
+	@Test
+	public void jeuDeuceEtAvantageTest() {
+		IntStream.range(0, 3).forEach(new IntConsumer() {
+			@Override
+			public void accept(int value) {
+				j1.gagnerPoint();
+				j2.gagnerPoint();
+			}
+		});
+
+		assertEquals("DEUCE", jeu.getStatus());
+
+		j2.gagnerPoint();
+		assertEquals("J2 doit avoir AVANTAGE", "AVANTAGE", jeu.getStatus());
+		j1.gagnerPoint();
+		assertEquals("Le jeu doit revenir a DEUCE", "DEUCE", jeu.getStatus());
+	}
+}
