1a3fccfc75496be9eb49fb3ddc280936276c6045
U959139 <aimed@hotmail.fr>
2016-10-31 15:17:51 +0100

    Adding Set test

---
+++ jeu.tennis/src/test/java/com/socgen/lyxor/kata/tennis/SetTest.java
@@ -0,0 +1,34 @@
+package com.socgen.lyxor.kata.tennis;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.socgen.lyxor.kata.tennis.impl.JoueurImpl;
+
+public class SetTest {
+
+	Joueur	j1	= new JoueurImpl("YMB");
+	Joueur	j2	= new JoueurImpl("LYX");
+	Set		set;
+
+	@Before
+	public void init() {
+		set = new SetImpl(j1, j2, 1);
+	}
+
+	@Test
+	public void jeuCourantEndTest() {
+		IntStream.range(0, 4).forEach(new IntConsumer() {
+			@Override
+			public void accept(int value) {
+				j1.gagnerPoint();
+			}
+		});
+		assertEquals(true, set.getJeuCourant().end());
+	}
+}
